06961c8d852c43195f639a27ac31cd712b516918
Astamur Ladariya <149709871+reques6e@users.noreply.github.com>
2024-08-03 12:00:53 +0300

    ⚡ Получение времени по ntp.cyxym.net

--- main.py
+++ main.py
@@ -12,28 +12,41 @@ from data.requests import get_locations, get_camera, get_timelapse
 from loguru import logger
 from config import Config
 
-def get_date():
+def get_ntp_time():
     c = ntplib.NTPClient()
-    response = c.request('ntp.cyxym.net')
-    ntp_time = datetime.utcfromtimestamp(response.tx_time) - timedelta(days=1)
-    return ntp_time.strftime("%Y-%m-%d")
+    response = c.request('pool.ntp.org')
+    return datetime.utcfromtimestamp(response.tx_time)
+
+def get_system_time():
+    return datetime.utcnow()
+
+def check_time_difference():
+    ntp_time = get_ntp_time()
+    system_time = get_system_time()
+    difference = abs((ntp_time - system_time).total_seconds())
+    difference = 444.7000
+    if difference > 300:  # 300 секунд это 5 минут
+        logger.error(f"Разница между системным временем ({system_time}) и NTP временем ({ntp_time}) больше пяти минут. Пожалуйста, исправьте системное время.")
+        exit(1)
+
+def get_date():
+    return (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
 
 def start_logging():
-    logger.add(f"data/logs/{get_date()}.log", rotation="00:00", retention="7 days", level="INFO") 
+    logger.add(f"data/logs/{get_date()}.log", rotation="00:00", retention="7 days", level="INFO")
+
+check_time_difference()
 
 start_logging()
 
-try:
-    s3_client = boto3.client(
-        's3',
-        endpoint_url=Config.endpoint_url,
-        aws_access_key_id=Config.aws_access_key_id,
-        aws_secret_access_key=Config.aws_secret_access_key,
-        region_name=Config.region_name
-    )
-except ValueError:
-    logger.error(f"Не заполнены данные в конфиге")
-    
+s3_client = boto3.client(
+    's3',
+    endpoint_url=Config.endpoint_url,
+    aws_access_key_id=Config.aws_access_key_id,
+    aws_secret_access_key=Config.aws_secret_access_key,
+    region_name=Config.region_name
+)
+
 async def start():
     logger.info(f"Запуск от {get_date()}")
 
